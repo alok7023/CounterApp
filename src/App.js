3e95dda0226ad99e525c1bf6ed79c3cce0111d11
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-01-20 01:28:22 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
   return (
-    <div className="App">
+    <div className="container">
       <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <div className="container-btn">
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
     </div>
   );
 }
